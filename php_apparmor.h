02afa2ea358f57ddbbbad51eb5129c072b9af37b
Felix Geyer <debfx@fobos.de>
2013-06-16 10:58:48 +0200

    Add apparmor.profile ini entry.

--- php_apparmor.h
+++ php_apparmor.h
@@ -40,27 +40,9 @@ PHP_RINIT_FUNCTION(apparmor);
 PHP_RSHUTDOWN_FUNCTION(apparmor);
 PHP_MINFO_FUNCTION(apparmor);
 
-PHP_FUNCTION(confirm_apparmor_compiled);	/* For testing, remove later. */
-
-/* 
-  	Declare any global variables you may need between the BEGIN
-	and END macros here:     
-
 ZEND_BEGIN_MODULE_GLOBALS(apparmor)
-	long  global_value;
-	char *global_string;
+    char* profile_name;
 ZEND_END_MODULE_GLOBALS(apparmor)
-*/
-
-/* In every utility function you add that needs to use variables 
-   in php_apparmor_globals, call TSRMLS_FETCH(); after declaring other 
-   variables used by that function, or better yet, pass in TSRMLS_CC
-   after the last function argument and declare your utility function
-   with TSRMLS_DC after the last declared argument.  Always refer to
-   the globals in your function as APPARMOR_G(variable).  You are 
-   encouraged to rename these macros something shorter, see
-   examples in any other php module directory.
-*/
 
 #ifdef ZTS
 #define APPARMOR_G(v) TSRMG(apparmor_globals_id, zend_apparmor_globals *, v)
